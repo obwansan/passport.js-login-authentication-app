bdba77b3eb69da67ceff5d1b59a0ffb5ade4a5e1
Kevin O'Brien <20611407+obwansan@users.noreply.github.com>
2019-07-07 09:43:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,9 @@ app.get("/", function(req, res) {
   res.render("home");
 });
 
-// When a a get request is sent to the "/secret" route, the isLoggedIn function is called before anything else happens (it's middleware). If the user's credentials are authenticated, the 'next' callback function is called, that renders the /secret page. If not, you're redirected back to the log in page and the 'next' function isn't called.
+// When a a get request is sent to the "/secret" route, the isLoggedIn function is called before anything else happens 
+// (it's middleware). If the user's credentials are authenticated, the 'next' callback function is called, that renders 
+// the /secret page. If not, you're redirected back to the log in page and the 'next' function isn't called.
 app.get("/secret", isLoggedIn, function(req, res) {
   res.render("secret");
 });
@@ -65,7 +67,8 @@ app.get("/register", function(req, res) {
 // handle user sign-up
 app.post("/register", function(req, res) {
   // Creates a new user in the database with the username and password.
-  // The register method hashes the password (why it's not set as a property on the object passed to register() ) so the actual password isn't stored in the database.
+  // The register method hashes the password (why it's not set as a property on the object passed to register() ) 
+  // so the actual password isn't stored in the database.
   // register() returns either an error object or user object to the callback.
   User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
     if(err) {
@@ -88,9 +91,13 @@ app.get("/login", function(req, res) {
 })
 
 // login logic
-// Submitting the form generates a post request to this app.post("/login") route. The form data (i.e. username and password) are available as properties on the request object passed to the route callback (e.g. req.body.username).
-// passport.authenticate() is middleware (i.e. code that runs between the request and response, or between the beginning and end of the route).
-// passport.authenticate() tries to log the user in. It gets the username and password off request.body and authenticates them.
+// Submitting the form generates a post request to this app.post("/login") route. 
+// The form data (i.e. username and password) are available as properties on the 
+// request object passed to the route callback (e.g. req.body.username). 
+// passport.authenticate() is middleware (i.e. code that runs between the request 
+// and response, or between the beginning and end of the route).
+// passport.authenticate() tries to log the user in. It gets the username and password 
+// off request.body and authenticates them.
 app.post("/login", passport.authenticate("local", {
   successRedirect: "/secret",
   failureRedirect: "/login"
@@ -110,7 +117,9 @@ function isLoggedIn(req, res, next) {
   res.redirect("/login");
 }
 
-// The app.listen() function creates the Node.js web server at the specified host and port (in this case localhost:3000). It is identical to Node's http.Server.listen() method (when you run the command 'node app.js' this listens on port 3000 so the 'base' for all routes is localhost:3000)
+// The app.listen() function creates the Node.js web server at the specified host and port (in this case localhost:3000). 
+// It is identical to Node's http.Server.listen() method (when you run the command 'node app.js' this listens on port 3000 
+// so the 'base' for all routes is localhost:3000)
 app.listen(3000, function() {
   console.log("server started.....");
 });
